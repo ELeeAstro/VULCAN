c852e2c22b7d4abb083a892bc5cb4f02852fb86a
Elspeth KH Lee <38210132+ELeeAstro@users.noreply.github.com>
2025-12-18 10:56:20 +0100

    update for PR

--- vulcan_cfg.py
+++ vulcan_cfg.py
@@ -3,10 +3,12 @@
 # ============================================================================= 
 
 # ====== Setting up the elements included in the network ======
-atom_list = ['H', 'O', 'P']
+atom_list = ['H', 'O', 'C', 'N',  'He']
 # ====== Setting up paths and filenames for the input and output files  ======
 # input:
-network = 'thermo/PHO_full_photo_network.txt'
+#network = 'thermo/SNCHO_full_photo_network.txt'
+
+network = 'thermo/NCHO_photo_network.txt'
 use_lowT_limit_rates = False
 gibbs_text = 'thermo/gibbs_text.txt' # (all the nasa9 files must be placed in the folder: thermo/NASA9/)
 cross_folder = 'thermo/photo_cross/'
@@ -15,21 +17,23 @@ atm_file = 'atm/atm_HD189_Kzz.txt' # TP and Kzz (optional) file
 sflux_file = 'atm/stellar_flux/sflux-HD189_Moses11.txt' # sflux-HD189_B2020.txt This is the flux density at the stellar surface
 top_BC_flux_file = 'atm/BC_top.txt' # the file for the top boundary conditions
 bot_BC_flux_file = 'atm/BC_bot.txt' # the file for the lower boundary conditions
-vul_ini = 'output/HD189-nominal.vul' # the file to initialize the abundances for ini_mix = 'vulcan_ini'
+vul_ini = 'output/' # the file to initialize the abundances for ini_mix = 'vulcan_ini'
 # output:
 output_dir = 'output/'
 plot_dir = 'plot/'
 movie_dir = 'plot/movie/'
-out_name =  'HD189_P_th_10x.vul' # output file name
+out_name =  'HD189.vul' # output file name
 
 # ====== Setting up the elemental abundance ======
-use_solar = False # True: using the solar abundance from Table 10. K.Lodders 2009; False: using the customized elemental abundance. 
-# customized elemental abundance (only read when use_solar = False) # Elsie Uses Aslpund et al. (2021)
-O_H = 4.897788193684457e-04 * 10.0
-P_H = 2.570395782768865e-07 * 10.0
-He_H = 0.08203515443298177
+use_solar = True # True: using the solar abundance from Table 8. K.Lodders 2019; False: using the customized elemental abundance. 
+# customized elemental abundance (only read when use_solar = False)
+O_H = 5.37E-4 #*(0.793)  
+C_H = 2.95E-4  
+N_H = 7.08E-5
+S_H = 1.41E-5
+He_H = 0.0838
 ini_mix = 'EQ' # Options: 'EQ', 'const_mix', 'vulcan_ini', 'table' (for 'vulcan_ini, the T-P grids have to be exactly the same)
-fastchem_met_scale = 10.0 # scaling factor for other elements in fastchem (e.g., if fastchem_met_scale = 0.1, other elements such as Si and Mg will take 0.1 solar values)
+fastchem_met_scale = 1. # scaling factor for other elements in fastchem (e.g., if fastchem_met_scale = 0.1, other elements such as Si and Mg will take 0.1 solar values)
 
 use_ini_cold_trap = True #True
 # Initialsing uniform (constant with pressure) mixing ratios (only reads when ini_mix = const_mix)
@@ -65,8 +69,8 @@ if use_photo == False and use_ion == True:
 # ====== Setting up parameters for the atmosphere ======
 atm_base = 'H2' #Options: 'H2', 'N2', 'O2', 'CO2 -- the bulk gas of the atmosphere: changes the molecular diffsion, thermal diffusion factor, and settling velocity
 rocky = False # for the surface gravity
-nz = 100   # number of vertical layers
-P_b = 1e8  # pressure at the bottom (dyne/cm^2)
+nz = 120   # number of vertical layers
+P_b = 1e9  # pressure at the bottom (dyne/cm^2)
 P_t = 1e-2 # pressure at the top (dyne/cm^2)
 use_Kzz = True
 use_moldiff = True
@@ -83,7 +87,7 @@ Tiso = 1000 # only read when atm_type = 'isothermal'
 # T_int, T_irr, ka_L, ka_S, beta_S, beta_L
 para_warm = [120., 1500., 0.1, 0.02, 1., 1.]
 para_anaTP = para_warm
-const_Kzz = 1.E9 # (cm^2/s) Only reads when use_Kzz = True and Kzz_prof = 'const'
+const_Kzz = 1.E10 # (cm^2/s) Only reads when use_Kzz = True and Kzz_prof = 'const'
 const_vz = 0 # (cm/s) Only reads when use_vz = True and vz_prof = 'const'
 
 # frequency for updating dz and dzi due to change of mu
@@ -94,8 +98,9 @@ update_frq = 100
 use_topflux = False
 use_botflux = False
 use_fix_sp_bot = {} # fixed mixing ratios at the lower boundary
-diff_esc = [] # species for diffusion-limit escape at TOA
+diff_esc = ['H'] # species for diffusion-limit escape at TOA
 max_flux = 1e13  # upper limit for the diffusion-limit fluxes
+use_sat_surfaceH2O = True
 
 # ====== Reactions to be switched off  ======
 remove_list = [] # in pairs e.g. [1,2]
@@ -139,13 +144,13 @@ mtol_conv = 1.E-20
 pos_cut = 0
 nega_cut = -1.
 loss_eps = 1e-1
-yconv_cri = 0.1 # for checking steady-state
-slope_cri = 1.e-3
+yconv_cri = 0.01 # for checking steady-state
+slope_cri = 1.e-4
 yconv_min = 0.1
 flux_cri = 0.1
 flux_atol = 1. # the tol for actinc flux (# photons cm-2 s-1 nm-1)
 ### use with caution
-conver_ignore = [] # added 2023. to get rid off non-convergent species, e.g. HC3N without sinks 
+conver_ignore = ['HC3N'] # added 2023. to get rid off non-convergent species, e.g. HC3N without sinks 
 
 # ====== Setting up numerical parameters for Ros2 ODE solver ====== 
 rtol = 0.25             # relative tolerence for adjusting the stepsize 
@@ -168,10 +173,7 @@ use_PIL = True
 live_plot_frq = 10
 save_movie_rate = live_plot_frq
 y_time_freq = 1  #  storing data for every 'y_time_freq' step
-#plot_spec = ['PH3','PH2','HOPO','HOPO2']
-#plot_spec = ['PH3','PH2','PH','HOPO','HOPO2','P2H2','P2H3','P2H4','H3PO4','OH','O','P4O6']
-plot_spec = ['PH3','PH2','PH','P','PO','PO2','HOPO','P2','P4','OH', 'P4O6']
-#plot_spec = ['PH3','PH2','PO','PO2','HOPO','H3PO4']
+plot_spec = ['H2O', 'H', 'CH4', 'CO', 'CO2', 'C2H2', 'HCN', 'NH3' ]
 # output:
 output_humanread = False
 use_shark = False
